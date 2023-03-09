25b6928f446a7fdd14d3b834169c0c0969fb49a1
Æterna <67712622+aeterna-gh@users.noreply.github.com>
2023-03-08 19:43:44 -0500

    upload 2
    Signed-off-by: Æterna <67712622+aeterna-gh@users.noreply.github.com>

---
+++ Content/Traits/origin/origin_Disassembler.cs
@@ -0,0 +1,28 @@
+﻿namespace AEF.Content.Traits.origin
+{
+    internal class origin_Disassembler : Trait
+    {
+        public override void SetDefaults()
+        {
+            myName = "Machina Disassembler";
+
+            Description = 
+                "MACHINA OFFENSIVE ORIGIN TRAIT \n" +
+                "Increases this weapon's damage against Machina enemies. \n\n" +
+                "Also increases: \n" +
+                "- Knight = Increased Break Chance \n" +
+                "- Mage = Increased Status Chance and Duration \n" +
+                "- Ranger = Increased Crit Chance and Damage";
+            base.SetDefaults();
+        }
+
+        public override void ConditionalEffect(KnightWeapon k, MageWeapon m, RangerWeapon r)
+        {
+            //if this weapon is hitting a Machina enemy, increase impact and increase respective stat
+
+            
+
+            base.ConditionalEffect(k, m, r);
+        }
+    }
+}
---
+++ Content/Traits/origin/origin_KeepTempo.cs
@@ -0,0 +1,20 @@
+﻿namespace AEF.Content.Traits.origin
+{
+    internal class origin_KeepTempo : Trait
+    {
+        public override void SetDefaults()
+        {
+            myName = "Keeping Tempo";
+            Description = "";
+
+            base.SetDefaults();
+        }
+
+        public override void ConditionalEffect(KnightWeapon k, MageWeapon m, RangerWeapon r)
+        {
+
+
+            base.ConditionalEffect(k, m, r);
+        }
+    }
+}
